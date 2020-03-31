4f84f16f4059b775740b916bd3ba03d5c3de7d0a
romilshah525 <romilshah525@gmail.com>
2020-03-31 23:56:46 +0530

    responsive for mobile

--- js/plots.js
+++ js/plots.js
@@ -413,3 +413,9 @@ $(".sidenav")
   .on("click tap", "li a", () => {
     $(".sidenav").sidenav("close");
   });
+if ($(window).width() <= 599) {
+  let a = document.getElementsByClassName("add-class");
+  for (i of a) {
+    i.classList.remove("container");
+  }
+}
