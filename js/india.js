0b368c493261cc261c29b88e95c56cf9a16eb529
romilshah525 <romilshah525@gmail.com>
2020-04-03 22:32:59 +0530

    bubble chart ready in temp.js, how to relate but??

--- js/india.js
+++ js/india.js
@@ -1,11 +1,14 @@
-let data, state_district_wise, state_daily, time_series_data;
-const ctx11 = document.getElementById("myChart11");
+let entireData, state_district_wise, state_daily, time_series_data, statewise;
+const ctx11 = document.getElementById("myChart11"),
+  ctx12 = document.getElementById("myChart12"),
+  ctx13 = document.getElementById("myChart13"),
+  ctx14 = document.getElementById("myChart14");
 fetch("https://api.covid19india.org/data.json")
   .then(res => res.json())
   .then(res => {
-    data = res;
+    entireData = res;
+    statewise = res.statewise;
     time_series_data = res.cases_time_series;
-    console.log(time_series_data);
     plotGraph((data = time_series_data));
   });
 
@@ -13,22 +16,17 @@ function plotGraph(data = entiredata, val = false, id = null) {
   let confirmed = [],
     deceased = [],
     recovered = [],
-    date = [],
-    totalDeceased = 0,
-    totalRecovered = 0,
-    totalConfirmed = 0;
+    date = [];
   data.forEach(element => {
     date.push(element["date"]);
     confirmed.push(Number(element["dailyconfirmed"]));
     deceased.push(Number(element["dailydeceased"]));
     recovered.push(Number(element["dailyrecovered"]));
   });
-  console.log(confirmed);
-
   myChart11 = new Chart(ctx11.getContext("2d"), {
     type: "bar",
     data: {
-      date,
+      labels: date,
       datasets: [
         {
           label: "Confirmed ",
@@ -77,6 +75,75 @@ function plotGraph(data = entiredata, val = false, id = null) {
         yAxes: [
           {
             display: true,
+            ticks: {
+              min: 0,
+              stepSize: 15
+            },
+            scaleLabel: {
+              display: true,
+              labelString: "Confirmed Count "
+            }
+          }
+        ]
+      }
+    }
+  });
+  myChart12 = new Chart(ctx12.getContext("2d"), {
+    type: "bar",
+    data: {
+      labels: date,
+      datasets: [
+        {
+          label: "Confirmed ",
+          data: confirmed,
+          fill: true,
+          backgroundColor: "#223e80a0",
+          hoverBorderColor: "#223e80ff",
+          borderColor: "#223e80ff"
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      legend: {
+        position: "bottom",
+        labels: {
+          fontColor: "#223e80ff"
+        }
+      },
+      title: {
+        display: true,
+        text: `Covid19 Confirmed Daily Count - India`
+      },
+      tooltips: {
+        mode: "index",
+        intersect: false
+      },
+      hover: {
+        mode: "nearest",
+        intersect: true
+      },
+      responsive: true,
+      chartArea: {
+        backgroundColor: "#223e8011"
+      },
+      scales: {
+        xAxes: [
+          {
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: "Date"
+            }
+          }
+        ],
+        yAxes: [
+          {
+            display: true,
+            ticks: {
+              min: 0,
+              stepSize: 15
+            },
             scaleLabel: {
               display: true,
               labelString: "Confirmed Count "
--- js/temp.js
+++ js/temp.js
@@ -1,69 +1,64 @@
-var config = {
-  type: "line",
-  data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-      {
-        label: "My First dataset",
-        backgroundColor: "red",
-        borderColor: "red",
-        data: [1, 2, 3, 4, 5, 6, 7],
-        fill: false
-      },
-      {
-        label: "My Secind dataset",
-        backgroundColor: "blue",
-        borderColor: "blue",
-        data: [8, 9, 10, 11, 12, 13, 14],
-        fill: false
-      }
-    ]
+function colorize(opaque, context) {
+  let value = context.dataset.data[context.dataIndex];
+  let x = value.x / 100;
+  let y = value.y / 100;
+  let r = x < 0 && y < 0 ? 250 : x < 0 ? 150 : y < 0 ? 50 : 0;
+  let g = x < 0 && y < 0 ? 0 : x < 0 ? 50 : y < 0 ? 150 : 250;
+  let b = x < 0 && y < 0 ? 0 : x > 0 && y > 0 ? 250 : 150;
+  let a = opaque ? 1 : (9.9 * value.r) / 1000;
+  return "rgba(" + r + "," + g + "," + b + "," + a + ")";
+}
+
+let data = [
+  { x: 10, y: 10, r: 30 },
+  { x: 20, y: 20, r: 30 },
+  { x: 30, y: 30, r: 30 },
+  { x: 40, y: 40, r: 30 },
+  { x: 50, y: 50, r: 30 },
+  { x: 60, y: 60, r: 30 },
+  { x: 70, y: 70, r: 30 },
+  { x: 80, y: 80, r: 30 },
+];
+data = { datasets: [{ data }] };
+let options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  legend: { position: "bottom", labels: { fontSize: 12 } },
+  title: { display: true, text: "Market Capital Share", fontSize: 18 },
+  scales: {
+    xAxes: [
+      { gridLines: false },
+      { scaleLabel: { display: true, labelString: "Total Amount (Rs.)" } },
+    ],
   },
-  options: {
-    responsive: true,
-    title: {
-      display: true,
-      text: "Chart.js Line Chart"
+  yAxes: [
+    { gridLines: false },
+    { scaleLabel: { display: true, labelString: "Average Amount (Rs.)" } },
+  ],
+  aspectRatio: 1,
+  elements: {
+    point: {
+      backgroundColor: colorize.bind(null, false),
+      borderColor: colorize.bind(null, true),
+      hoverBorderWidth: 6,
+      // hoverBackgroundColor: "transparent",
+      borderWidth: function (context) {
+        return Math.min(Math.max(1, context.datasetIndex + 1), 8);
+      },
+      hoverBorderColor: function (context) {
+        let value = context.dataset.data[context.dataIndex];
+        return Math.round((0.8 * value.r) / 1000);
+      },
+      hoverBorderWidth: function (context) {
+        let value = context.dataset.data[context.dataIndex];
+        return Math.round((0.99 * value.r) / 7);
+      },
     },
-    tooltips: {
-      mode: "index",
-      intersect: false
-    },
-    hover: {
-      mode: "nearest",
-      intersect: true
-    },
-    scales: {
-      xAxes: [
-        {
-          display: true,
-          scaleLabel: {
-            display: true,
-            labelString: "Month"
-          }
-        }
-      ],
-      yAxes: [
-        {
-          display: true,
-          scaleLabel: {
-            display: true,
-            labelString: "Value"
-          },
-          ticks: {
-            min: 0,
-            max: 100,
-
-            // forces step size to be 5 units
-            stepSize: 5
-          }
-        }
-      ]
-    }
-  }
+  },
 };
 
-window.onload = function() {
-  var ctx = document.getElementById("myChart11").getContext("2d");
-  window.myLine = new Chart(ctx, config);
-};
+let chart = new Chart("myChart13", {
+  type: "bubble",
+  data: data,
+  options: options,
+});
