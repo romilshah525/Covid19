438dba481f9c53012a044d602e628bafc45f3d97
romilshah525 <romilshah525@gmail.com>
2020-04-06 12:22:57 +0530

    minor updates

--- js/india.js
+++ js/india.js
@@ -17,13 +17,13 @@ const ctx11 = document.getElementById("myChart11").getContext("2d"),
 let country = "India";
 
 fetch("https://api.covid19india.org/data.json")
-  .catch(fetch("../india.json"))
+  .catch(fetch("Covid19/india.json"))
   .then((res) => res.json())
   .then((res) => {
     entireData = res;
     statewise = res.statewise;
     time_series_data = res.cases_time_series;
-    // plotBubbleChart();
+    printSummary();
     plotGraph((data = time_series_data));
   });
 
@@ -40,9 +40,7 @@ function plotGraph(data = entiredata, val = false, id = null) {
     recovered.push(Number(element["dailyrecovered"]));
     active.push(
       Number(element["dailyconfirmed"]) -
-        (Number(element["dailyrecovered"]) +
-          Number(element["dailydeceased"]) +
-          Number(element["dailyrecovered"]))
+        (Number(element["dailyrecovered"]) + Number(element["dailydeceased"]))
     );
   });
   myChart11 = new Chart(ctx11, {
@@ -223,102 +221,29 @@ function plotGraph(data = entiredata, val = false, id = null) {
   });
 }
 
-function plotBubbleChart() {
-  let data = [],
-    total = 1;
-  c = 1;
-  statewise.forEach((element) => {
-    if (element.state != "Total") {
-      data.push({
-        x: c,
-        y: element.confirmed,
-        r: element.confirmed / total + 10,
-      });
-      c += 2;
-    } else {
-      total = element.confirmed;
-      totalActiveCovidOrg = element.active;
-      totalConfirmedCovidOrg = element.confirmed;
-      totalDeathsCovidOrg = element.deaths;
-      totalReceoveredCovidOrg = element.recovered;
-      document.getElementById(
-        "total-confirmed-cases"
-      ).innerText = `Positives: ${totalConfirmedCovidOrg}`;
-      document.getElementById(
-        "total-recovered-cases"
-      ).innerText = `Recoveries: ${totalReceoveredCovidOrg}`;
-      document.getElementById(
-        "total-active-cases"
-      ).innerText = `Active: ${totalActiveCovidOrg}`;
-      document.getElementById(
-        "total-death-cases"
-      ).innerText = `Deaths: ${totalDeathsCovidOrg}`;
-      document.getElementById(
-        "recovery-rate"
-      ).innerText = `Recovery Rate: ${String(
-        (totalReceoveredCovidOrg / totalConfirmedCovidOrg) * 100
-      ).slice(0, 5)}%`;
-      document.getElementById("death-rate").innerText = `Death Rate: ${String(
-        (totalDeathsCovidOrg / totalConfirmedCovidOrg) * 100
-      ).slice(0, 5)}%`;
-    }
-  });
-
-  function colorize(opaque, context) {
-    let value = context.dataset.data[context.dataIndex];
-    let x = value.x / 100,
-      y = value.y / 100,
-      r = x < 0 && y < 0 ? 250 : x < 0 ? 150 : y < 0 ? 50 : 0,
-      g = x < 0 && y < 0 ? 0 : x < 0 ? 50 : y < 0 ? 150 : 250,
-      b = x < 0 && y < 0 ? 0 : x > 0 && y > 0 ? 250 : 150,
-      a = opaque ? 1 : (9.9 * value.r) / 100;
-    return "rgba(" + r + "," + g + "," + b + "," + a + ")";
-  }
-  data = { datasets: [{ data }] };
-  let options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    tootips: false,
-    legend: { position: "bottom", labels: { fontSize: 12 } },
-    title: {
-      display: true,
-      text: "Covid 19 Confirmed Cases in Indian States",
-      fontSize: 18,
-    },
-    scales: {
-      xAxes: [
-        { gridLines: false },
-        { scaleLabel: { display: true, labelString: "State" } },
-      ],
-    },
-    yAxes: [
-      { gridLines: false },
-      { scaleLabel: { display: true, labelString: "Confirmed Cases" } },
-    ],
-    aspectRatio: 1,
-    elements: {
-      point: {
-        backgroundColor: colorize.bind(null, false),
-        borderColor: colorize.bind(null, true),
-        hoverBorderWidth: 6,
-        borderWidth: function (context) {
-          return Math.min(Math.max(1, context.datasetIndex + 1), 8);
-        },
-        hoverBorderColor: function (context) {
-          let value = context.dataset.data[context.dataIndex];
-          return Math.round((0.8 * value.r) / 1000);
-        },
-        hoverBorderWidth: function (context) {
-          let value = context.dataset.data[context.dataIndex];
-          return Math.round((0.99 * value.r) / 7);
-        },
-      },
-    },
-  };
-
-  let bubbleChart = new Chart(ctx15, {
-    type: "bubble",
-    data: data,
-    options: options,
-  });
+function printSummary() {
+  element = statewise[0];
+  total = element.confirmed;
+  totalActiveCovidOrg = element.active;
+  totalConfirmedCovidOrg = element.confirmed;
+  totalDeathsCovidOrg = element.deaths;
+  totalReceoveredCovidOrg = element.recovered;
+  document.getElementById(
+    "total-confirmed-cases"
+  ).innerText = `Positives: ${totalConfirmedCovidOrg}`;
+  document.getElementById(
+    "total-recovered-cases"
+  ).innerText = `Recoveries: ${totalReceoveredCovidOrg}`;
+  document.getElementById(
+    "total-active-cases"
+  ).innerText = `Active: ${totalActiveCovidOrg}`;
+  document.getElementById(
+    "total-death-cases"
+  ).innerText = `Deaths: ${totalDeathsCovidOrg}`;
+  document.getElementById("recovery-rate").innerText = `Recovery Rate: ${String(
+    (totalReceoveredCovidOrg / totalConfirmedCovidOrg) * 100
+  ).slice(0, 5)}%`;
+  document.getElementById("death-rate").innerText = `Death Rate: ${String(
+    (totalDeathsCovidOrg / totalConfirmedCovidOrg) * 100
+  ).slice(0, 5)}%`;
 }
