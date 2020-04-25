c6e4ed476975807b29a3d22e59f077f9cd24d7d5
romilshah525 <romilshah525@gmail.com>
2020-04-26 01:23:10 +0530

    resources integrated

--- js/resources.js
+++ js/resources.js
@@ -42,18 +42,13 @@ function update() {
   let listHTML = ``;
   data.forEach((element) => {
     if (element.state == state && (cat == "All" || element.category == cat)) {
-      listHTML += ` <li>
-            <div class="collapsible-header">
-            <span>${element.state}</span>
-        </div>
-        <div class="collapsible-body row">
-            <h6 class="col s12 m6 flow-text"><a href="${element.contact}" target="_blank">${element.nameoftheorganisation}</a></h6>
-            <h6 class="col s12 m6 flow-text">Category:${element.category}</h6>
-            <h6 class="col s12 m6 flow-text">City:${element.city}</h6>
-            <h6 class="col s12 m6 flow-text">Contact: ${element.phonenumber}</h6>
-            <p class="col s12">${element.descriptionandorserviceprovided}</p>
-        </div>
-    </li>`;
+      listHTML += `<tr>
+      <td><a href="${element.contact}" target="_blank">${element.nameoftheorganisation}</a></td>
+      <td>${element.category}</td>
+      <td>${element.phonenumber}</td>
+      <td>${element.city}</td>
+      <td>${element.state}</td>
+  </tr>`;
     }
   });
   document.getElementById("list-data").innerHTML = listHTML;
--- js/script.js
+++ js/script.js
@@ -10,16 +10,4 @@ $(".sidenav")
 $(document).ready(function () {
   $(".tabs").tabs();
   $(".collapsible").collapsible();
-  // M.toast({
-  //   html: "View in Desktop for visualizing graphs",
-  //   inDuration: 1000,
-  //   outDuration: 1000,
-  //   displayLength: 50000,
-  //   classes: "hide-on-med-and-up",
-  //   activationPercent: 0.25,
-  // });
-  // $(".tap-target").tapTarget();
-  // if ($(window).width() < 600) {
-  //   $(".tap-target").tapTarget("open");
-  // }
 });
