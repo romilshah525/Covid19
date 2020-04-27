67c7d85e531516cb410a85e2087608f40ddf4205
romilshah525 <romilshah525@gmail.com>
2020-04-28 00:09:01 +0530

    analysis 2-2

--- js/analysis.js
+++ js/analysis.js
@@ -1,23 +1,51 @@
 let genderCount = {
-  M: 0,
-  F: 0,
-  "": 0,
-};
-let entireData;
-let ageCount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-let canvas1 = document.getElementById("canvas1").getContext("2d");
-let canvas2 = document.getElementById("canvas2").getContext("2d");
+    M: 0,
+    F: 0,
+    "": 0,
+  },
+  countryList = {},
+  globalEntireData,
+  rawEntireData,
+  selectedCountryList,
+  ageCount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  chart1,
+  chart2,
+  chart3;
+const colors = ["#fc3903", "#f2b438", "#87c93a", "#3ac96e", "#3ac9b6"];
+let canvas1 = document.getElementById("canvas1").getContext("2d"),
+  canvas2 = document.getElementById("canvas2").getContext("2d"),
+  canvas3 = document.getElementById("canvas3").getContext("2d");
+
+let elems = document.querySelectorAll(".chips-autocomplete");
+let instance = M.Chips.init(elems, {
+  data: [
+    { tag: "India" },
+    { tag: "China" },
+    { tag: "Spain" },
+    { tag: "Italy" },
+  ],
+  autocompleteOptions: {
+    data: countryList,
+    limit: 5,
+    minLength: 1,
+  },
+  onChipAdd: function () {
+    plotCountryWiseChart(instance[0].chipsData);
+  },
+  onChipDelete: function () {
+    plotCountryWiseChart(instance[0].chipsData);
+  },
+});
+
 function readData() {
-  console.log("Page loaded!");
+  // fetch("https://api.covid19india.org/raw_data.json")
   fetch("https://api.covid19india.org/raw_data.json")
-    // .catch((err) => fetch("Covid19/json/india.json"))
     .then((res) => res.json())
     .then(({ raw_data }) => {
       let age,
         ageNotDisclosed = 0;
-      entireData = raw_data;
+      rawEntireData = raw_data;
       raw_data.forEach((element) => {
-        console.log(element.agebracket);
         genderCount[element.gender] += 1;
         age = Number(element.agebracket);
         if (element.agebracket == "") {
@@ -50,12 +78,6 @@ function readData() {
         type: "doughnut",
         data: {
           datasets: [
-            // {
-            //   data: [genderCount["M"], genderCount["F"], genderCount[""]],
-            //   backgroundColor: ["#eb54e877", "#6354eb77", "#c4cbcc77"],
-            //   borderColor:["#eb54e8ff", "#6354ebff", "#c4cbccff"],
-            //   borderWidth:[1,1,1],
-            // },
             {
               data: [genderCount["M"], genderCount["F"]],
               backgroundColor: ["#eb54e877", "#6354eb77"],
@@ -63,22 +85,13 @@ function readData() {
               borderWidth: [1, 1],
             },
           ],
-          // labels: ["Male", "Female", `NA-${genderCount[""]}`],
           labels: ["Male", "Female"],
         },
         options: {
           responsive: true,
-          legend: {
-            position: "bottom",
-          },
-          title: {
-            display: true,
-            text: "Patients Count - GenderWise",
-          },
-          animation: {
-            animateScale: true,
-            animateRotate: true,
-          },
+          legend: { position: "bottom" },
+          title: { display: true, text: "Patients Count - GenderWise" },
+          animation: { animateScale: true, animateRotate: true },
         },
       };
       let chart1 = new Chart(canvas1, config1);
@@ -97,7 +110,6 @@ function readData() {
             "80-90",
             "90-100",
             "100+",
-            // `NA-${ageNotDisclosed}`
           ],
           datasets: [
             {
@@ -111,12 +123,25 @@ function readData() {
         },
         options: {
           responsive: true,
-          legend: {
-            position: "top",
-          },
+          legend: { display: false },
           title: {
             display: true,
-            text: "Patient Count - Agewise",
+            text: `Patients Count - Agewise`,
+            fontSize: 14,
+          },
+          tooltips: { mode: "index", intersect: false },
+          hover: { mode: "nearest", intersect: true },
+          scales: {
+            xAxes: [{ ticks: { fontSize: 12 } }],
+            yAxes: [
+              {
+                ticks: {
+                  fontSize: 12,
+                  callback: (value) =>
+                    value > 800 ? String(value / 1000) + "k" : value,
+                },
+              },
+            ],
           },
         },
       };
@@ -127,5 +152,95 @@ function readData() {
       document.getElementById(
         "canvas2note"
       ).innerText = `Age values unavailable for ${ageNotDisclosed} patients.`;
+    })
+    .then((res) => fetch("https://pomber.github.io/covid19/timeseries.json"))
+    .catch((err) => fetch("Covid19/json/global.json"))
+    .then((response) => response.json())
+    .then((data) => {
+      Object.keys(data).forEach((element) => (countryList[element] = null));
+      globalEntireData = data;
+      plotCountryWiseChart(instance[0].chipsData);
     });
+}
+
+function plotCountryWiseChart(countries) {
+  console.log(countries);
+  if (!countries.length) {
+    chart3="";
+  }
+  if (window.screen.availWidth <= 650) size = 6;
+  else size = 14;
+  let datasets = [];
+  let labels = [];
+  globalEntireData["India"].forEach((el) =>
+    labels.push(el["date"].split("-").reverse().join("-").slice(0, 4))
+  );
+  let i = 0;
+  countries.forEach((country) => {
+    let data = [];
+    globalEntireData[country.tag].forEach((el) => data.push(el.confirmed));
+    datasets.push({
+      label: country.tag,
+      data: data,
+      fill: false,
+      backgroundColor: colors[i] + "ff",
+      borderColor: colors[i] + "ff",
+      pointRadius: 0,
+    });
+    i += 1;
+  });
+  let options = {
+    type: "line",
+    data: { labels, datasets: datasets },
+    options: {
+      responsive: true,
+      legend: {
+        display: false,
+        position: "bottom",
+        labels: { fontColor: "#222", fontSize: size },
+      },
+      title: {
+        display: true,
+        text: `Covid19 Cases Count`,
+        fontSize: size + 4,
+      },
+      tooltips: { mode: "index", intersect: false },
+      hover: { mode: "nearest", intersect: true },
+      chartArea: { backgroundColor: "#223e8011" },
+      scales: {
+        xAxes: [{ gridLines: { display: false }, ticks: { fontSize: 9 } }],
+        yAxes: [
+          {
+            gridLines: { display: false },
+            ticks: {
+              fontSize: 9,
+              callback: (value) =>
+                value > 800 ? String(value / 1000) + "k" : value,
+            },
+          },
+        ],
+      },
+    },
+  };
+  if (size > 12) {
+    options.options.legend.display = true;
+    options.options.scales.xAxes = [
+      {
+        display: true,
+        scaleLabel: { display: true, labelString: "Date" },
+      },
+    ];
+    options.options.scales.yAxes = [
+      {
+        display: true,
+        scaleLabel: { display: true, labelString: "Count " },
+        ticks: {
+          fontSize: 12,
+          callback: (value) =>
+            value > 800 ? String(value / 1000) + "k" : value,
+        },
+      },
+    ];
+  }
+  chart3 = new Chart(canvas3, options);
 }
