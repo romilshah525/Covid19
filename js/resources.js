08134586156d72f3c742637188cf02572cfd3ed9
romilshah525 <romilshah525@gmail.com>
2020-05-16 10:56:53 +0530

    resources.js code cleaned

--- js/resources.js
+++ js/resources.js
@@ -1,28 +1,19 @@
 let data = [],
   zoneCompleteData = [],
-  districtZoneColorData = {},
+  state_district_wise = {},
   colorForZone = {
     Red: "#ff0d0d",
     Orange: "#ff8a0d",
     Green: "#78d669",
   };
 
-function readData() {
-  fetch("https://api.covid19india.org/resources/resources.json")
-    .then((res) => res.json())
-    .then(({ resources }) => renderResourcesDetails(resources))
-    .then((res) => fetch("https://api.covid19india.org/zones.json"))
-    .catch((err) => fetch("Covid19/json/zones.json"))
-    .then((response) => response.json())
-    .then(({ zones }) => zoneUpdate(zones));
-}
-
 function renderResourcesDetails(resources) {
   data = resources;
-  let states = [];
-  let categories = [];
-  let categoryHTML = `<option value="All">All</option>`;
-  let stateHTML = "";
+  let states = [],
+    categories = [],
+    stateHTML = `<option value="" disabled selected>Choose your State</option>`,
+    categoryHTML = `<option value="All">All</option>`;
+
   resources.forEach((element) => {
     states.push(element.state);
     categories.push(element.category);
@@ -35,44 +26,17 @@ function renderResourcesDetails(resources) {
   states.forEach((st) => (stateHTML += `<option value="${st}">${st}</option>`));
   document.getElementById("category").innerHTML = categoryHTML;
   document.getElementById("state").innerHTML = stateHTML;
-  var elem = document.querySelector(".collapsible.expandable");
-  var instance = M.Collapsible.init(elem, {
-    accordion: false,
-  });
+  const elem = document.querySelector(".collapsible.expandable");
+  const instance = M.Collapsible.init(elem, { accordion: false });
   $(document).ready(function () {
     $("select").formSelect();
   });
   document.getElementById("to-be-updated").remove();
-  document.getElementById("state").value = "Andaman & Nicobar";
-  updateResourcesTable();
-}
-
-function zoneUpdate(zones) {
-  zoneCompleteData = zones;
-  function GetSortOrder(prop) {
-    return function (a, b) {
-      if (a[prop] > b[prop]) return 1;
-      else if (a[prop] < b[prop]) return -1;
-      return 0;
-    };
-  }
-  zones.sort(GetSortOrder("district"));
-  let data = ``;
-  zones.forEach((zone) => {
-    data += `<option value="${zone.districtcode}">${zone.district}</option>`;
-    districtZoneColorData[zone.districtcode] = zone.zone;
-  });
-  let selectHTML = document.getElementById("zone-state");
-  selectHTML.innerHTML = data;
-  $(document).ready(function () {
-    $("select").formSelect();
-  });
-  updateZoneColor();
 }
 
 function updateResourcesTable() {
-  let state = document.getElementById("state").value;
-  let cat = document.getElementById("category").value;
+  const state = document.getElementById("state").value,
+    cat = document.getElementById("category").value;
   let listHTML = ``;
   data.forEach((element) => {
     if (element.state == state && (cat == "All" || element.category == cat)) {
@@ -82,9 +46,50 @@ function updateResourcesTable() {
   document.getElementById("list-data").innerHTML = listHTML;
 }
 
+function zoneData(zones) {
+  zones.forEach((zone) => (state_district_wise[zone.state] = {}));
+  zones.forEach(
+    (zone) => (state_district_wise[zone.state][zone.district] = zone.zone)
+  );
+
+  let data = `<option value="" disabled selected>Choose your State</option>`,
+    temp = Object.keys(state_district_wise);
+  temp.forEach((t) => (data += `<option value="${t}">${t}</option>`));
+  document.getElementById("zone-state").innerHTML = data;
+  $(document).ready(function () {
+    $("select").formSelect();
+  });
+}
+
+function selectDistrict() {
+  let data = `<option value="" disabled selected>Choose your District</option>`,
+    temp = Object.keys(
+      state_district_wise[document.getElementById("zone-state").value]
+    );
+
+  temp.forEach((t) => (data += `<option value="${t}">${t}</option>`));
+  document.getElementById("zone-district").innerHTML = data;
+  $(document).ready(function () {
+    $("select").formSelect();
+  });
+}
+
 function updateZoneColor() {
-  let value = document.getElementById("zone-state").value;
+  const state = document.getElementById("zone-state").value;
+  const district = document.getElementById("zone-district").value;
   document.getElementById("zone-name-text").innerHTML = `<h5 style="color: ${
-    colorForZone[districtZoneColorData[value]]
-  } !important; font-weight: bolder;">${districtZoneColorData[value]}</h5>`;
+    colorForZone[state_district_wise[state][district]]
+  } !important; font-weight: bolder;">${
+    state_district_wise[state][district]
+  }</h5>`;
+}
+
+function readData() {
+  fetch("https://api.covid19india.org/resources/resources.json")
+    .then((res) => res.json())
+    .then(({ resources }) => renderResourcesDetails(resources))
+    .then((res) => fetch("https://api.covid19india.org/zones.json"))
+    .catch((err) => fetch("Covid19/json/zones.json"))
+    .then((response) => response.json())
+    .then(({ zones }) => zoneData(zones));
 }
